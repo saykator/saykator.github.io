b2e740937cc91ab79f5a31e5927df0982eb11396
heliacer <heliacer35@gmail.com>
2024-10-01 22:22:43 +0200

    initial commit add dependecies, configuration

---
+++ README.md
@@ -0,0 +1,16 @@
+# saykator.github.io
+
+## Installation
+
+*Development*
+```
+npm i
+npm run dev
+```
+
+*Production*
+```
+npm ci
+npm run build
+npm run preview
+```
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { faFlask } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+const App = () => {
+  return (
+    <>
+      <FontAwesomeIcon icon={faFlask} /> Work in progress
+    </>
+  )
+}
+
+export default App
